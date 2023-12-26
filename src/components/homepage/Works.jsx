90f91e075e638c2c4ec18903842327834ad929f5
Yash <yashsrivastava7526@gmail.com>
2023-12-26 21:02:48 +0530

    project

--- src/components/homepage/Works.jsx
+++ src/components/homepage/Works.jsx
@@ -28,52 +28,7 @@ export default function Works({ forwardedRef }) {
 
           />
         </div>
-        {/* Project #2 */}
-        {/* <div className="col-span-1 pt-0 md:col-span-7 md:pt-16">
-          <Projects
-            link="https://mementostudio.netlify.app/"
-            img={memento}
-            alt="memento landing page mockup"
-            name="memento studio landing page"
-            type="Frontend Development"
-            year="2023"
-            tools="HTML • TailwindCSS • JavaScript"
-          />
-        </div>
-        <div className="col-span-1 pt-0 md:col-span-5 md:pt-80">
-          <Projects
-            link="https://realbusinessaccountants.netlify.app"
-            img={acc}
-            alt="real business accountant project mockup"
-            name="real business accountants"
-            type="Web Design • Frontend Development"
-            year="2023"
-            tools="HTML • CSS • JavaScript • Figma"
-          />
-        </div>
-        <div className="col-span-1 h-fit pt-0 md:col-span-8 md:pt-20">
-          <Projects
-            link="https://godaddyuiclone.netlify.app"
-            img={daddy}
-            alt="godaddy clone page mockup"
-            name="GODADDY LANDING PAGE CLONE"
-            type="Frontend Development"
-            year="2023"
-            tools="HTML • TailwindCSS • JavaScript"
-          />
-        </div>
-        <div className="col-span-1 h-fit md:col-span-4">
-        <Projects
-            link="https://sunnysidechallenge.netlify.app"
-            img={sunnyside}
-            alt="sunnyside project mockup"
-            name="SUNNYSIDE LANDING PAGE"
-            type="Frontend Development"
-            year="2022"
-            tools="HTML • TailwindCSS • JavaScript"
-          />
-         
-        </div> */}
+  
       </div>
     </section>
   );
